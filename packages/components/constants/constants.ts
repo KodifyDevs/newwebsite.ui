55e18580c20445147441a5b59d17a4b3ba5640ef
Juan Morales <juancarlosmoralestellez@gmail.com>
2024-04-23 14:16:25 -0600

    Creation of the interface, constants folders.

---
+++ packages/components/constants/constants.ts
@@ -0,0 +1,9 @@
+import { NavItem } from "../interfaces/NavItem";
+
+export const navItems: NavItem[] = [
+  { id: 1, text: 'Inicio' },
+  { id: 2, text: 'Nosotros' },
+  { id: 3, text: 'Servicios' },
+  { id: 4, text: 'Proyectos' },
+  { id: 5, text: 'Blog' }
+];
