9ba76224bb4128338bce181e8dedf277c99fd68f
Juan Morales <juancarlosmoralestellez@gmail.com>
2024-05-14 19:58:15 -0600

    fix style button componenet

--- packages/components/src/Button/Button.tsx
+++ packages/components/src/Button/Button.tsx
@@ -5,10 +5,10 @@ export const Button: React.FC<ButtonProps> = ({ textColor, bgColor, bgHover, tex
     const IconComponent = dynamicIcon && bsIcon[dynamicIcon];
     
     const defaultTextColor = textColor || 'text-white';
-    const defaultBgColor = bgColor || 'bg-richBlack';
+    const defaultBgColor = bgColor || 'bg-rich-black';
     const defaultBgHover = bgHover || 'hover:bg-white'
-    const defaultBorderColor = borderColor || 'hover:border-richBlack'
-    const defaultTextHover = textHover || 'hover:text-richBlack'
+    const defaultBorderColor = borderColor || 'hover:border-rich-black'
+    const defaultTextHover = textHover || 'hover:text-rich-black'
 
     return (
         <a href="" className={`font-bold flex gap-x-2 px-[77px] p-[14px] leading-5 ${defaultTextColor} ${defaultTextHover} group flex justify-center items-center w-auto h-auto rounded-[10px] ${defaultBgColor} mx-auto ${defaultBgHover} hover:border hover:border-solid ${defaultBorderColor}`}>
