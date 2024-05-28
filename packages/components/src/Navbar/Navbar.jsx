45eb060895f9a2b6a59b3914187ff43ebc4e71f2
Juan Morales <juancarlosmoralestellez@gmail.com>
2024-05-27 18:02:40 -0600

    position correction from 75px to 68px

--- packages/components/src/Navbar/Navbar.jsx
+++ packages/components/src/Navbar/Navbar.jsx
@@ -167,7 +167,7 @@ const Navbar = ({ navItems }) => {
         {/* Mobile Navigation item */}
         <div
           className={`flex flex-col fixed items-center h-[300px] left-0 ${nav 
-            ? ' top-[75px] w-[100%] bg-white ease-in-out duration-500 -z-10 lg:hidden '
+            ? ' top-[67px] w-[100%] bg-white ease-in-out duration-500 -z-10 lg:hidden '
             : ' w-[100%] top-[-100%] ease-in-out duration-500 z-10'
             } 
         ${isSecondOpen ? 'h-[410px]' : 'h-[300px]'} 
@@ -175,7 +175,7 @@ const Navbar = ({ navItems }) => {
         >
           <ul
             className={`flex px-[116px] pb-6 pt-2 flex-col items-center fixed left-auto right-auto w-10/12 ${nav
-              ? 'rounded-xl top-[75pxj] h-[280px] bg-rich-black-10 ease-in-out duration-500 lg:hidden'
+              ? 'rounded-xl top-[67px] h-[280px] bg-rich-black-10 ease-in-out duration-500 lg:hidden'
               : 'h-[280px] top-[-100%] ease-in-out duration-500'
               }
           ${isSecondOpen ? 'h-[347px]' : ''} 
