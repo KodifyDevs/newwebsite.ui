494abe745cf620fd238068744823faecd0443513
Juan Morales <juancarlosmoralestellez@gmail.com>
2024-05-29 16:31:39 -0600

    Style order arrangement, and addition of a bg-very-light-gray

--- apps/web/containers/Hero.jsx
+++ apps/web/containers/Hero.jsx
@@ -4,14 +4,14 @@ import Image from "next/image";
 export const Hero = () => {
     return (
         <section>
-          <div className="w-full bg-bgMobile">
-            <div className="max-w-[1553px] lg:h-[580px] xl:flex xl:mx-auto ">
+          <div className="w-full bg-very-light-gray">
+            <div className="max-w-[1553px] lg:h-[580px] xl:flex xl:mx-auto">
               <div className="w-fit mx-auto lg:mt-[140px]">
                 <div className="px-4 pt-6 text-left xl:w-[537px]">
                   <h1 className="text-title-clamp font-bold text-rich-black leading-leading-title xl:leading-leading-title-desk">
                     Crece junto a nosotros
                   </h1>
-                  <h2 className="mt-4 font-semibold text-2xl leading-leading-7 md:text-3xl lg:text-3xl md:leading-10 text-rich-black ">y alcanza el éxito que te mereces</h2>
+                  <h2 className="mt-4 font-semibold text-2xl leading-leading-7 text-rich-black md:text-3xl md:leading-10 lg:text-3xl ">y alcanza el éxito que te mereces</h2>
   
                   <div className="text-paragraph-clamp mt-4">
                     <p className="font-medium text-lg leading-5 text-rich-black-80 ">
@@ -24,7 +24,7 @@ export const Hero = () => {
                       Descúbrenos
                     </Button>
                   </div>
-                  <div className="hidden md:block ml-5">
+                  <div className="hidden ml-5 md:block">
                     <Button 
                     backgroundColor="var(--green)"
                     backgroundColorHover="var(--white)"
@@ -44,7 +44,7 @@ export const Hero = () => {
     
               </div>
     
-              <div className="hidden xl:block relative h-fit top-[-276px] right-[-5px] ">
+              <div className="hidden relative h-fit top-[-276px] right-[-5px] xl:block">
                 <Image
                 src="/Kodify_ilustration.png"
                 width={700}
