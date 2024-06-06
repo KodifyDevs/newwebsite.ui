e2448b9cbae2e181bb6e902be240879a6323c222
Juan Morales <juancarlosmoralestellez@gmail.com>
2024-06-06 12:29:07 -0600

    Card Component

---
+++ packages/components/src/Card/Card.jsx
@@ -0,0 +1,82 @@
+"use client";
+import Button from "../Button/Button";
+import Container from "../Container";
+const Card = ({
+   frontBgColor = "var(--baby-powder)",
+   backBgColor = "var(--secondary-color)",
+   frontFontColor = "var(--primary-color)",
+   backfontColor = "var(--baby-powder)",
+   frontDropShadow = '0px 4px 15px 0px var(--primary-color-10)',
+   backdropShadow = '0px 4px 15px 0px var(--secondary-color-15)',
+   radius = '10px',
+   cardData
+}) => {
+   return (
+      <div className="w-full bg-cultured">
+         <Container>
+            <div className="py-12 lg:mx-auto xl:px-[136px]">
+               <h2 className="text-center font-bold text-3xl leading-leading-title text-black">Servicios</h2>
+               {/* Card container */}
+               <div className="flex flex-wrap gap-y-6 gap-x-[75px] justify-center my-6 mx-auto">
+                  {cardData.map((data) => (
+                     // Card front
+                     <article key={data.id} className="group [perspective:1000px]">
+                        <section className="card__front relative px-[63px] py-[99px] transition-all duration-500 lg:[transform-style:preserve-3d] lg:group-hover:[transform:rotateY(180deg)]">
+                           <div className="relative inset-0">
+                              <img className="mx-auto w-[50[x] h-[50px]" src={data.img.flip} alt={data.imgAlt.flip} />
+                              <div className="w-[195px] h-[76px] text-center mt-3">
+                                 <h2 className="text-3xl font-bold leading-leading-title">
+                                    {data.title}
+                                 </h2>
+                              </div>
+                           </div>
+                           {/* card back */}
+                           <section className="card__back absolute inset-0 px-[17px] py-[75px] text-center [backface-visibility:visible] lg:[transform:rotateY(180deg)] lg:[backface-visibility:hidden]">
+                              <div className="flex flex-col items-center justify-center">
+                                 <img className="mx-auto w-[50[x] h-[50px]" src={data.img.base} alt={data.imgAlt.base} />
+                                 <h2 className="text-xl font-bold">{data.title}</h2>
+                                 <p className="text-base leading-[19.2px] mt-[26px]">{data.description}</p>
+                              </div>
+                           </section>
+                        </section>
+                     </article>
+                  ))}
+               </div>
+               <div className="w-fit mx-auto">
+                  <Button>
+                     Ver más
+                  </Button>
+               </div>
+            </div>
+         </Container>
+
+         {/* Custom Card Styles */}
+         <style jsx>
+            {`
+         .card__front {
+            background: ${frontBgColor};
+            border-radius: ${radius};
+            box-shadow: ${frontDropShadow};
+            color: ${frontFontColor};
+            width: 315px;
+         }
+            
+         .group:not(:hover) .card__front {
+         transition-delay: 500ms;
+         }
+
+         .card__back {
+            background: ${backBgColor};
+            border-radius: ${radius};
+            box-shadow: ${backdropShadow};
+            color: ${backfontColor};
+            width: 315px;
+         }      
+         `}
+         </style>
+      </div>
+
+   )
+}
+
+export default Card;
