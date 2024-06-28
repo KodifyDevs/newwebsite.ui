80820a6fc2236963edee076236f69a5f6a813c05
Juan Morales <juancarlosmoralestellez@gmail.com>
2024-06-28 11:21:56 -0600

    feat/web-66: Data about our work

--- apps/web/constants/index.js
+++ apps/web/constants/index.js
@@ -1,3 +1,4 @@
 export {default as navItems} from './NavData'
 export {default as cardData} from './CardData'
 export {default as servicesData} from './ServicesData'
+export {default as ourWorkData} from './ourWorkData'
---
+++ apps/web/constants/ourWorkData/ourWorkData.js
@@ -0,0 +1,28 @@
+const ourWorkData = [
+    {
+        id: 1,
+        title: "Proyecto",
+        img: '/Projects.png',
+        imgAlt: 'Projects',
+        description: "Lorem ipsum dolor sit amet consectetur. Massa dictum vulputate suspendisse congue. Pellentesque sit neque a morbi vitae urna tincidunt congue.",
+
+    },
+    {
+        id: 2,
+        title: "Proyecto",
+        img: '/Projects.png',
+        imgAlt: 'Projects',
+        description: "Lorem ipsum dolor sit amet consectetur. Massa dictum vulputate suspendisse congue. Pellentesque sit neque a morbi vitae urna tincidunt congue.",
+
+    },
+    {
+        id: 3,
+        title: "Proyecto",
+        img: '/Projects.png',
+        imgAlt: 'Projects',
+        description: "Lorem ipsum dolor sit amet consectetur. Massa dictum vulputate suspendisse congue. Pellentesque sit neque a morbi vitae urna tincidunt congue.",
+
+    },
+]
+
+export default ourWorkData;
