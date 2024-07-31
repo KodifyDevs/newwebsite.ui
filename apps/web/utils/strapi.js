03b862fad450efe7a76ab28ca093b108bd7f5057
dierish05 <dierish.a.rojas@gmail.com>
2024-07-31 13:49:11 -0600

    hacer una sola funcion

--- apps/web/utils/strapi.js
+++ apps/web/utils/strapi.js
@@ -1,41 +1,11 @@
 import { STRAPI_URL } from "../config/config";
 
-export async function getHome() {
-    const res = await fetch(`${STRAPI_URL}/api/home?populate=*`)
-    if (!res.ok) {
-      throw new Error('something went wrong')
-    }
-  
-    const { data } = await res.json()
-    return data
+export async function getData(url) {
+  const res = await fetch(`${STRAPI_URL}${url}`)
+  if (!res.ok) {
+    throw new Error('something went wrong')
   }
 
-  export async function getAboutUs() {
-    const res = await fetch(`${STRAPI_URL}/api/about-us?populate=*`)
-    if (!res.ok) {
-      throw new Error('something went wrong')
-    }
-  
-    const { data } = await res.json()
-    return data
-  }
-
-  export async function getService() {
-    const res = await fetch(`${STRAPI_URL}/api/service?populate=*`)
-    if (!res.ok) {
-      throw new Error('something went wrong')
-    }
-  
-    const { data } = await res.json()
-    return data
-  }
-
-  export async function getProject() {
-    const res = await fetch(`${STRAPI_URL}/api/project?populate=*`)
-    if (!res.ok) {
-      throw new Error('something went wrong')
-    }
-  
-    const { data } = await res.json()
-    return data
-  }
+  const { data } = await res.json()
+  return data
+}
